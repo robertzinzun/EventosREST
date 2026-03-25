c0cb27f9652bfb57107301c9b82c129ca7f5ed80
Roberto Suarez Zinzun <31252552+robertzinzun@users.noreply.github.com>
2026-03-25 09:15:09 -0600

    Preparación de rutas para la API de Eventos

--- APiRest/main.py
+++ APiRest/main.py
@@ -6,7 +6,15 @@ app=FastAPI()
 @app.get("/")
 def home():
     return "Bienvenido a la APIRest de Eventos"
-
+@app.post("/eventos")
+async def crearEvento():
+    return {"mensaje":"Creando un evento"}
+@app.get("/eventos")
+async def listarEventos():
+    return {"mensaje":"Listando eventos"}
+@app.get("/eventos/{idEvento}")
+def listarEvento(idEvento:int):
+    return {"mensaje":f"Consultando el evento con id:{idEvento}"}
 if __name__ == '__main__':
    uvicorn.run("main:app",reload=True)
 
