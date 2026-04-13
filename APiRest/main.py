4679dba28d91372d551be6beb661cda17345f45e
Roberto Suarez Zinzun <31252552+robertzinzun@users.noreply.github.com>
2026-04-13 08:26:05 -0600

    Descripcion de las operaciones
    Cambios con las configuraciones de los elementos tags y summary

--- APiRest/main.py
+++ APiRest/main.py
@@ -3,18 +3,22 @@ import uvicorn
 
 app=FastAPI()
 
-@app.get("/")
+@app.get("/",tags=["Inicio"],summary="Home")
 def home():
     return "Bienvenido a la APIRest de Eventos"
-@app.post("/eventos")
+@app.post("/eventos",tags=["Eventos"],summary="Crear Evento")
 async def crearEvento():
     return {"mensaje":"Creando un evento"}
-@app.get("/eventos")
+@app.get("/eventos",tags=["Eventos"],summary="Listar Eventos")
 async def listarEventos():
     return {"mensaje":"Listando eventos"}
-@app.get("/eventos/{idEvento}")
-def listarEvento(idEvento:int):
+@app.get("/eventos/{idEvento}",tags=["Eventos"],summary="Listar Evento")
+def listarEvento(idEvento:str):
     return {"mensaje":f"Consultando el evento con id:{idEvento}"}
+
+@app.put("/eventos/{idEvento}",tags=["Eventos"],summary="Modificar evento en base a su ID")
+def modificarEvento(idEvento:str):
+    return {"mensaje":f"Modificando el evento con id:{idEvento}"}
 if __name__ == '__main__':
    uvicorn.run("main:app",reload=True)
 
